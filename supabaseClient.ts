9bcf6c0e394e971148444951aa298510ac67c0eb
rodrigosantos-sudo <rodrigo.santos@10xglobal.com.br>
2025-12-03 16:31:54 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'COLE_SUA_URL_DO_SUPABASE_AQUI'
-const supabaseKey = 'COLE_SUA_CHAVE_ANON_PUBLIC_AQUI'
+const supabaseUrl = 'https://kdazrscugoninpyrmnsm.supabase.co'
+const supabaseKey = 'sb_publishable_DKIGoum1-9c4f2Iqj1h3jA_ve8p2fOe'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
